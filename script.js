7907c72835983fd192018ec961168a9492bc74de
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-05-23 17:27:43 -0300

    feat: Instantiating variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const passInput = document.querySelector("#inputPasswordId");
+const lenInput = document.querySelector("#inputLengthId");
+const infoLength = document.querySelector("label[for='inputLengthId']");
+const btnGerar = document.querySelector("#btnGerar");
